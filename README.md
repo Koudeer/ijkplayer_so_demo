e4f92e1d1f5f8ab42df1bf3185473c1b22c9f594
Koudeer <koudeer@gmail.com>
2021-05-24 04:47:05 +0800

    更新支持rtsp协议

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 全架构SO文件
 
-支持HTTPS协议
+支持HTTPS/rtsp/rtmp协议
 
 感谢哔哩哔哩的工程师
--- app/src/main/java/com/koudeer/ijkplayer_so_demo/MainActivity.java
+++ app/src/main/java/com/koudeer/ijkplayer_so_demo/MainActivity.java
@@ -12,14 +12,36 @@ public class MainActivity extends AppCompatActivity {
 
     private IjkVideoView mView;
 
+    private String url =
+            "https://vd2.bdstatic.com/mda-igwdmz0nv8ah95xv/mda-igwdmz0nv8ah95xv.mp4?playlist=%5B%22hd%22%2C%22sc%22%5D&auth_key=1567068990-0-0-003a15194906c2b947cf493eec963a7f&bcevod_channel=searchbox_feed&pd=bjh";
+    private String rtmp = "rtmp://58.200.131.2:1935/livetv/hunantv";
+
+    private String rtsp = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mView = findViewById(R.id.ijk_video);
 
-        mView.setVideoURI(Uri.parse("https://jzvd.nathen.cn/video/cfe6c30-1767b1bc21f-0007-1823-c86-de200.mp4"));
+        mView.setVideoURI(Uri.parse(rtsp));
 
         mView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mView.isPlaying()){
+            mView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mView.canPause()){
+            mView.resume();
+        }
+        super.onResume();
     }
 }
